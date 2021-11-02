c7741c0295477eb1e5d680cfd953b29c7cbaaff1
danny <dannypolyakov95@gmail.com>
2021-11-02 20:13:19 +0200

    dev_br - solution to upload file

--- download_file.py
+++ download_file.py
@@ -15,10 +15,8 @@ try:
     input3 = browser.find_element_by_name('email')
     input3.send_keys("test@mail.com")
 
-    choose_file = browser.find_element_by_css_selector("[accept='.txt']")
-    choose_file.click()
-
-    current_dir = os.path.abspath(os.path.dirname(__file__))  # получаем путь к директории текущего исполняемого файла
+    choose_file = browser.find_element_by_css_selector("[type='file']")
+    current_dir = os.path.abspath('/home/danny/Projects/selenium_pytest')  # получаем путь к директории текущего исполняемого файла
     file_path = os.path.join(current_dir, 'file.txt')  # добавляем к этому пути имя файла
     choose_file.send_keys(file_path)
 
@@ -30,13 +28,6 @@ try:
     # ждем загрузки страницы
     time.sleep(1)
 
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
-
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
